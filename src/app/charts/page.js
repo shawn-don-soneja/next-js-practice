5fcd1af1f9f07327ce5b555f5ec4eb7a43ae88d5
Shawn Soneja <shawn.soneja85@gmail.com>
2024-12-09 11:29:24 -0500

    Updating messaging -- adding link to my writeup and website

--- src/app/charts/page.js
+++ src/app/charts/page.js
@@ -133,8 +133,13 @@ const Page = (props) => {
       <Row>
         <Col>
           <h1 className="mt-4 m-3">Financial Dashboard</h1>
+          <h5 className="mt-2 m-3 text-muted" style={{lineHeight:"1.5"}}>
+            Here we’re tracking four key macroeconomic indicators: <b>GDP, Unemployment, Interest Rates, and Inflation</b>. 
+            This project is a part of <a href='shawnsoneja.com' target='_blank'>shawnsoneja.com</a>. You can read more about it <a href='https://humorous-drink-54e.notion.site/Macro-Predictor-Project-Writeup-139b0558046180779d5dfa8641ed15f5' target='_blank'>here</a>.
+          </h5>
+
           <h5 className="mt-2 m-3 text-muted">
-            Here we’re tracking four key macroeconomic indicators: <b>GDP, Unemployment, Interest Rates, and Inflation</b>
+            
           </h5>
         </Col>
       </Row>
