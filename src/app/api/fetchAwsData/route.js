5f058e95b42382d7ed7eafb9bbfee4266a4e8b48
Shawn Soneja <shawn.soneja85@gmail.com>
2025-05-26 01:52:57 -0400

    trying to allow auth on my protected page's back-end call

--- src/app/api/fetchAwsData/route.js
+++ src/app/api/fetchAwsData/route.js
@@ -1,3 +1,6 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/auth.config"; // Adjust path if needed
+
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
@@ -5,6 +8,15 @@ const ddbClient = new DynamoDBClient({ region: process.env.AWS_DEFAULT_REGION })
 const ddb = DynamoDBDocumentClient.from(ddbClient);
 
 export async function GET() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new Response(JSON.stringify({ error: "Not authenticated" }), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
   try {
     const data = await ddb.send(new ScanCommand({ TableName: "Automated_Process_Logs" }));
     console.log(data);
