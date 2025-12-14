5043d407eacbf9fa224eed0c4a330edadf3fdc7b
Shawn Soneja <shawn.soneja85@gmail.com>
2025-12-14 18:49:03 -0500

    refactor: removing unused logo image

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,6 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <h1>Shawn wuz here</h1>
         <ol>
           <li><a href="/macro-dashboard">/macro-dashboard</a></li>
