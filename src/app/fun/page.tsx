6d56a4b2cb64f27595f5eeb36beac7f843d5e4b1
Shawn Soneja <shawn.soneja85@gmail.com>
2026-01-02 00:01:34 -0500

    feat: updates for "fun" page - adding some links and things I want to cater for perusing

--- src/app/fun/page.tsx
+++ src/app/fun/page.tsx
@@ -14,32 +14,49 @@ export default function FunPage() {
             </Col>
         </Row>
         <Row>
-            <Col className="mb-4 d-inline-block justify-content-center" lg={true}>
+            <Col className="mb-4 d-inline-block justify-content-center" xs={12} md={6}>
                 <Card className={styles.card}>
-                    <p className="m-3">
-                        Just gonna wanna make some cells for the latest top 3 videos from:
+                    
+                    <h3 className={styles.header}>
+                        MIT Open Courseware
                         <br/>
-                        <a href="https://www.youtube.com/@mitocw" target="_blank">MIT Open Courseware</a>
+                        <a href="https://www.youtube.com/@mitocw" target="_blank">
+                            <Badge bg="secondary" className={styles.badgeText}>Youtube Channel↗</Badge>
+                        </a>
+                    </h3>
+                    <p className="m-3">
+                        Just gonna wanna make some cells for the latest top 3 videos from the MIT Courseware channel
                     </p>
                 </Card>
             </Col>
-            <Col className="mb-4 d-inline-block justify-content-center" lg={true}>
+            <Col className="mb-4 d-inline-block justify-content-center" xs={12} md={6}>
                 <Card className={styles.card}>
-                    <p className="m-3">
+                    <h3 className={styles.header}>
                         Latest articles from Aljazeera
                         <br/>
-                        <a href="" target="_blank">Al Jazeera</a>
+                        <a href="https://www.aljazeera.com/" target="_blank">
+                            <Badge bg="secondary" className={styles.badgeText}>News Feed↗</Badge>
+                        </a>
+                    </h3>
+                    <p className="m-3">
+                        Latest global news and updates
                     </p>
                 </Card>
             </Col>
         </Row>
         <Row>
-            <Col className="mb-4 d-inline-block justify-content-center" lg={true}>
+            <Col className="mb-4 d-inline-block justify-content-center" xs={12} md={6}>
                 <Card className={styles.card}>
-                    <h3 className={styles.header}>Youtube Playlist Links: <Badge bg="secondary" className={styles.badgeText}></Badge></h3>
+                    <h3 className={styles.header}>
+                        Youtube Playlist Links: 
+                        <br/>
+                        <a href="https://www.youtube.com/playlist?list=WL" target="_blank">
+                            <Badge bg="secondary" className={styles.badgeText}>Watch Later↗</Badge>
+                        </a>
+                    </h3>
                 </Card>
             </Col>
-            <Col className="mb-4 d-inline-block justify-content-center" lg={true}>
+            <Col className="mb-4 d-inline-block justify-content-center" xs={12} md={6}>
                 <Card className={styles.card}>
                     <h3 className={styles.header}>Random Ideas?<Badge bg="secondary" className={styles.badgeText}></Badge></h3>
                     <p>could be random trackers from looperman, for cakewalk</p>
@@ -50,12 +67,12 @@ export default function FunPage() {
             </Col>
         </Row>
         <Row>
-            <Col className="mb-4 d-inline-block justify-content-center" lg={true}>
+            <Col className="mb-4 d-inline-block justify-content-center" xs={12} md={6}>
                 <Card className={styles.card}>
                     <h3 className={styles.header}>Quick Notes (persistent... supports like <i>bookmarks</i><Badge bg="secondary" className={styles.badgeText}></Badge></h3>
                 </Card>
             </Col>
-            <Col className="mb-4 d-inline-block justify-content-center" lg={true}>
+            <Col className="mb-4 d-inline-block justify-content-center" xs={12} md={6}>
                 <Card className={styles.card}>
                     <h3 className={styles.header}>Add drag and drop reordering? + Reset<Badge bg="secondary" className={styles.badgeText}></Badge></h3>
                 </Card>
